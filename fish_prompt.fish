cad227e2bdc23faf8a5d427e1eb5f6816fc2590b
co2y <co2y@foxmail.com>
2016-12-23 19:51:56 +0800

    fix git branch name

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -3,23 +3,13 @@ function fish_prompt
     and printf (set_color red)(whoami)(set_color white)'@'(set_color yellow)(hostname)' '
 
 
-
-    test $USER = 'root'
-    and echo (set_color red)"#"
-
-    # Main
-    echo -n (set_color cyan)(prompt_pwd) (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
-    if set -q VIRTUAL_ENV
-        echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
-    end
-
 	if git rev-parse ^/dev/null
 		# Magenta if branch detached else green
 		git branch -qv | grep "\*" | string match -rq detached
 		and set_color brmagenta
 		or set_color brgreen
 
-		printf (git name-rev --name-only HEAD)' '
+		printf (git branch ^/dev/null | grep \* | sed 's/* //')' '
 
 		for i in (git branch -qv --no-color|grep \*|cut -d' ' -f4-|cut -d] -f1|tr , \n)\
  (git status --porcelain | cut -c 1-2 | uniq)
@@ -43,5 +33,16 @@ function fish_prompt
             end
         end
 	end
+
+
+    test $USER = 'root'
+    and echo (set_color red)"#"
+
+    # Main
+    echo -n (set_color cyan)(prompt_pwd) (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+    if set -q VIRTUAL_ENV
+        echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+    end
+
 
 end
